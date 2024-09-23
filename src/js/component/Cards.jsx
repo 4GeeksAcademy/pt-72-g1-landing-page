e612daea9763cf5bf7b47270405cc88b09015056
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-09-23 23:49:53 +0000

    Cards initialized

---
+++ src/js/component/Cards.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+
+export default function Cards(props) {
+    
+    return (
+        <div className = "container-fluid d-flex flex-row justify-content-center pt-5">
+            
+        <div className="card" style={{"width": "18rem"}}>
+                <img src={props.img} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary d-flex justify-content-center">Find out Now!</a>
+            </div>
+
+        </div>
+        
+        <div className="card" style={{"width": "18rem"}}>
+                <img src={props.img} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary d-flex justify-content-center">Find out Now!</a>
+            </div>
+        </div>
+        
+        <div className="card" style={{"width": "18rem"}}>
+                <img src={props.img} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary d-flex justify-content-center">Find out Now!</a>
+            </div>
+        </div>
+        
+        <div className="card" style={{"width": "18rem"}}>
+                <img src={props.img} className="card-img-top" alt="..."></img>
+            <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary d-flex justify-content-center">Find out Now!</a>
+            </div>
+        </div>
+    
+      </div>
+     
+    )
+}
+
