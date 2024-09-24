cb17c5a7e92fa575c87980aad254aebd95670a1c
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-09-24 00:04:52 +0000

    initialized

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -4,8 +4,9 @@ import React from "react";
 export default function Cards(props) {
     
     return (
-        <div className = "container-fluid d-flex flex-row justify-content-center pt-5">
-            
+        <div className = "d-flex flex-row justify-content-center align-items-end pt-3 mx-auto">
+
+        
         <div className="card" style={{"width": "18rem"}}>
                 <img src={props.img} className="card-img-top" alt="..."></img>
             <div className="card-body">
@@ -13,8 +14,8 @@ export default function Cards(props) {
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-primary d-flex justify-content-center">Find out Now!</a>
             </div>
-
-        </div>
+        </div>  
+        
         
         <div className="card" style={{"width": "18rem"}}>
                 <img src={props.img} className="card-img-top" alt="..."></img>
